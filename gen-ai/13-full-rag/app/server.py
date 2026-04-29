1c0f726ba152799302ec1056c18b9d4d2e3e79ad
Bhupesh1x <bhupeshvyas202@gmail.com>
2026-04-29 23:23:44 +0530

    feat: Added new endpoint for upload file and added logic for saving the file in the file path defined using file id and name

--- gen-ai/13-full-rag/app/server.py
+++ gen-ai/13-full-rag/app/server.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from uuid import uuid4
+
+from fastapi import FastAPI, UploadFile
+
+from .utils.file import save_to_desk
 
 app = FastAPI(title="full-rag")
 
 @app.get("/")
 def status():
   return {"status": "Health OK!"}
+
+@app.post("/upload")
+async def upload_file(file: UploadFile):
+  
+  id = uuid4()
+  
+  file_path = f"./mnt/uploads/{id}/{file.filename}"
+  
+  await save_to_desk(file=await file.read(), path=file_path)
+  
+  return { "file_id": id }
