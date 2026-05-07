e3644216b668b620fac64c43ae3a2a3f864117bf
Bhupesh1x <bhupeshvyas202@gmail.com>
2026-05-07 19:20:27 +0530

    feat: Added new endpoint for search based on the knowledge base using llm created using the docs uploaded in vector db

--- gen-ai/14-ai-support/app/server.py
+++ gen-ai/14-ai-support/app/server.py
@@ -1,7 +1,9 @@
 import os
 
 from uuid import uuid4
+from pydantic import BaseModel
 from dotenv import load_dotenv
+from langchain_groq import ChatGroq
 from qdrant_client import QdrantClient
 from langchain_qdrant import QdrantVectorStore
 from fastapi import FastAPI, UploadFile, HTTPException, File
@@ -18,6 +20,12 @@ QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
 QDRANT_COLLECTION_NAME = os.getenv("QDRANT_COLLECTION_NAME")
 QDRANT_VECTOR_NAME = os.getenv("QDRANT_VECTOR_NAME")
 
+QUERY_LIMIT = 3
+
+class QueryRequest(BaseModel):
+  query: str
+  
+
 app = FastAPI(title="ai-support")
 
 embeddings = GeminiEmbeddings()
@@ -27,9 +35,76 @@ client = QdrantClient(
     api_key=QDRANT_API_KEY
 )
 
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    api_key=GROQ_API_KEY
+)
+
 @app.get("/")
 def status():
   return {"status": "Health OK!"}
+
+@app.post('/ask')
+def ask(payload: QueryRequest):
+  
+  if not payload.query:
+    raise HTTPException(status_code=400, detail="Question is required")
+  
+  try:
+    vector_store = QdrantVectorStore(
+      client=client,
+      collection_name=QDRANT_COLLECTION_NAME,
+      embedding=embeddings,
+      vector_name=QDRANT_VECTOR_NAME
+    )
+    
+    # Semantic similarity search
+    results = vector_store.similarity_search_with_score(
+      query=payload.query,
+      k=QUERY_LIMIT
+    )
+    
+    docs = []
+    context_parts = []
+    
+    for doc, score in results:
+      docs.append({
+          "content": doc.page_content,
+          "metadata": doc.metadata,
+          "score": score
+    })
+      
+    context_parts.append(doc.page_content)
+    
+    # Combine all retrieved chunks
+    context = "\n\n".join(context_parts)  
+    
+    # Prompt for Groq
+    prompt = f"""
+      You are helpful AI assistant.
+
+      Answer the user's question ONLY using the provided context.
+
+      If the answer is not present in the context,
+      say: "I could not find the answer in the uploaded documents."
+      
+      Context: {context}
+      
+      Question: {payload.query}
+
+      Answer:
+    """
+    ai_response = llm.invoke(prompt)
+      
+    return {
+      "query": payload.query,
+      "results": ai_response.content,
+      "sources": docs
+    }  
+    
+  except Exception as e:
+    raise HTTPException(status_code=500, detail=str(e))  
+  
 
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):  
